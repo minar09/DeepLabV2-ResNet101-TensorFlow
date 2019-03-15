0ab79ef290cbc0eed9f9336671915a430e5e0258
MatiurRahmanMinar <minar09.bd@gmail.com>
2019-03-15 11:00:23 +0900

    init

---
+++ deeplab_resnet/utils.py
@@ -0,0 +1,107 @@
+from PIL import Image
+import numpy as np
+import os
+import tensorflow as tf
+
+# colour map
+label_colours = [(0, 0, 0)                 # 0=background
+                 # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+                 # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+                 # 11=diningtable, 12=dog, 13=horse, 14=motorbike, 15=person
+                 , (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128), (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0), (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128), (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)]
+# 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+
+
+def decode_labels(mask, num_images=1, num_classes=21):
+    """Decode batch of segmentation masks.
+
+    Args:
+      mask: result of inference after taking argmax.
+      num_images: number of images to decode from the batch.
+      num_classes: number of classes to predict (including background).
+
+    Returns:
+      A batch with num_images RGB images of the same size as the input. 
+    """
+    n, h, w, c = mask.shape
+    assert(n >= num_images), 'Batch size %d should be greater or equal than number of images to save %d.' % (
+        n, num_images)
+    outputs = np.zeros((num_images, h, w, 3), dtype=np.uint8)
+    for i in range(num_images):
+        img = Image.new('RGB', (len(mask[i, 0]), len(mask[i])))
+        pixels = img.load()
+        for j_, j in enumerate(mask[i, :, :, 0]):
+            for k_, k in enumerate(j):
+                if k < num_classes:
+                    pixels[k_, j_] = label_colours[k]
+        outputs[i] = np.array(img)
+    return outputs
+
+
+def prepare_label(input_batch, new_size, num_classes, one_hot=True):
+    """Resize masks and perform one-hot encoding.
+
+    Args:
+      input_batch: input tensor of shape [batch_size H W 1].
+      new_size: a tensor with new height and width.
+      num_classes: number of classes to predict (including background).
+      one_hot: whether perform one-hot encoding.
+
+    Returns:
+      Outputs a tensor of shape [batch_size h w 21]
+      with last dimension comprised of 0's and 1's only.
+    """
+    with tf.name_scope('label_encode'):
+        # as labels are integer numbers, need to use NN interp.
+        input_batch = tf.image.resize_nearest_neighbor(input_batch, new_size)
+        # reducing the channel dimension.
+        input_batch = tf.squeeze(input_batch, squeeze_dims=[3])
+        if one_hot:
+            input_batch = tf.one_hot(input_batch, depth=num_classes)
+    return input_batch
+
+
+def inv_preprocess(imgs, num_images, img_mean):
+    """Inverse preprocessing of the batch of images.
+       Add the mean vector and convert from BGR to RGB.
+
+    Args:
+      imgs: batch of input images.
+      num_images: number of images to apply the inverse transformations on.
+      img_mean: vector of mean colour values.
+
+    Returns:
+      The batch of the size num_images with the same spatial dimensions as the input.
+    """
+    n, h, w, c = imgs.shape
+    assert(n >= num_images), 'Batch size %d should be greater or equal than number of images to save %d.' % (
+        n, num_images)
+    outputs = np.zeros((num_images, h, w, c), dtype=np.uint8)
+    for i in range(num_images):
+        outputs[i] = (imgs[i] + img_mean)[:, :, ::-1].astype(np.uint8)
+    return outputs
+
+
+def load(saver, sess, ckpt_path):
+    '''Load trained weights.
+
+    Args:
+      saver: TensorFlow saver object.
+      sess: TensorFlow session.
+      ckpt_path: path to checkpoint file with parameters.
+    '''
+    ckpt = tf.train.get_checkpoint_state(ckpt_path)
+    if ckpt and ckpt.model_checkpoint_path:
+        saver.restore(sess, ckpt.model_checkpoint_path)
+        print("Restored model parameters from {}".format(ckpt_path))
+
+
+def save(saver, sess, logdir, step):
+    model_name = 'model.ckpt'
+    checkpoint_path = os.path.join(logdir, model_name)
+
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+
+    saver.save(sess, checkpoint_path, global_step=step)
+    print('The checkpoint has been created.')
